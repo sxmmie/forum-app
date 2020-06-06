81876adaf27617c9b6ebe1656bd059cb77cb9682
Sxmmie <samuelumohjnr@gmail..com>
2020-06-06 16:41:12 +0100

    Added Service layer
    Added an abstraction for the ForumService.

---
+++ forum-app.Data/IForum.cs
@@ -0,0 +1,23 @@
+﻿using forum_app.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace forum_app.Data
+{
+    // The ppurpose to define the behaviour of the ForumService or
+    // ANy type of service that might want to interact with the forum data in the DB
+    public interface IForum
+    {
+        Forum GetId(int id);
+        IEnumerable<Forum> GetAll();
+        IEnumerable<ApplicationUser> GetAllActiveUsers();
+
+        Task Create(Forum forum);
+        Task Delete(int forumId);
+        Task UpdateForumTitle(int forumId, string newTitle);
+        Task UpdateForumDescription(int forumId, string newDescription);
+    }
+}
---
+++ forum-app.Service/ForumService.cs
@@ -0,0 +1,57 @@
+﻿using forum_app.Data;
+using forum_app.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace forum_app.Service
+{
+    // Query DB for data and serve to the web project
+    public class ForumService : IForum
+    {
+        private readonly ApplicationDbContext _ctx;
+
+        public ForumService(ApplicationDbContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        public Task Create(Forum forum)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(int forumId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Forum> GetAll()
+        {
+            // Get all forums including posts
+            return _ctx.Forums.Include(forum => forum.Posts);
+        }
+
+        public IEnumerable<ApplicationUser> GetAllActiveUsers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Forum GetId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateForumDescription(int forumId, string newDescription)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateForumTitle(int forumId, string newTitle)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
